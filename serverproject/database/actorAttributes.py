9bbd7ede5519663f73c9879057446c99aa9f85e3
luixiao1223 <luixiao1223@sina.com>
2017-07-25 07:36:22 +0800

    add+edit

---
+++ serverproject/database/actorAttributes.py
@@ -0,0 +1,9 @@
+# -*- coding: GBK -*-
+
+class ActorAttributes(object):
+    def __init__(self):
+        super(ActorAttributes, self).__init__()
+        self.accoutFilePath = './database/actor-attr.txt'
+        return
+
+
